436c1b86adc34d4f156d86a96cd955f8a7f1ee00
Lynn-Ngx <C15426672@mydit.ie>
2020-05-18 13:28:21 +0100

    JWT implemented for logging in users

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const connectDB = require('./config/db');
 
 const app = express()
 
-//Connect Database
+// Connect Database
 connectDB()
+
+// Init middleware
+app.use(express.json({ extended: false }))
 
 app.get('/', (req, res) =>
     res.json({ msg: 'Welcome to the Contact Keeper API...'})
