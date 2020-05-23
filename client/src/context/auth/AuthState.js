b0a1fb3215043e7efc66ef06a76972ca251ab410
Lynn-Ngx <C15426672@mydit.ie>
2020-05-23 16:11:43 +0100

    Register User to DB

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import axios from 'axios'
 import AuthContext from './authContext';
 import authReducer from './authReducer';
 import {
@@ -24,15 +25,46 @@ const AuthState = (props) => {
     const [state, dispatch] = useReducer(authReducer, initialState)
 
     // Load user
+    const loadUser = () => {
+        console.log('load user')
+    }
 
     // Register user
+    const register = async formData => {
+        const config = {
+            header: {
+                'Content-Type': 'application/json'
+            }
+        }
+        try {
+            const res = await axios.post('/api/users', formData, config)
+
+            dispatch({
+                type: REGISTER_SUCCESS,
+                payload: res.data
+            })
+        }catch (err) {
+            dispatch({
+                type: REGISTER_FAIL,
+                payload: err.response.data.msg
+            })
+        }
+    }
 
     // Login user
+    const login = () => {
+        console.log('login')
+    }
 
     // Logout
+    const logout = () => {
+        console.log('logout')
+    }
 
     //Clear error
-
+    const clearErrors = () => {
+        dispatch({ type: CLEAR_ERRORS })
+    }
 
     return <AuthContext.Provider
         value={{
@@ -40,7 +72,12 @@ const AuthState = (props) => {
             isAuthenticated: state.isAuthenticated,
             loading: state.loading,
             user: state.user,
-            error: state.error
+            error: state.error,
+            register,
+            loadUser,
+            login,
+            logout,
+            clearErrors
         }}
     >{props.children} </AuthContext.Provider>
 }
